5ed980f066fb86bf05c14f7a63d8c7029fa09f0e
Javier A <70771192+Espryth@users.noreply.github.com>
2021-10-22 10:15:55 -0500

    move adventure adapter to individual module and create new bukkit adventure module

--- adventure/src/main/java/dev/henko/message/adventure/messenger/AdventureMessenger.java
+++ adventure/src/main/java/dev/henko/message/adventure/messenger/AdventureMessenger.java
@@ -1,9 +1,9 @@
-package dev.henko.message.bukkit.messenger;
+package dev.henko.message.adventure.messenger;
 
 import dev.henko.message.api.AbstractMessenger;
 import dev.henko.message.api.MessengerConfig;
 import dev.henko.message.api.source.Source;
-import dev.henko.message.bukkit.replacer.ComponentMessageReplacer;
+import dev.henko.message.adventure.replacer.ComponentMessageReplacer;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.minimessage.MiniMessage;
@@ -14,42 +14,47 @@ import java.util.Collections;
 import java.util.List;
 
 public class AdventureMessenger
-  extends AbstractMessenger<TextComponent> {
+  extends AbstractMessenger<Component> {
 
   private final MiniMessage miniMessage;
 
   public AdventureMessenger(
-    MessengerConfig<TextComponent> config,
+    MessengerConfig<Component> config,
     MiniMessage miniMessage
   ) {
     super(new ComponentMessageReplacer(), config);
     this.miniMessage = miniMessage;
   }
 
+  public AdventureMessenger(
+    MessengerConfig<Component> config
+  ) {
+    this(config, MiniMessage.get());
+  }
 
   @Override
-  public @NotNull TextComponent get(String path, Object... replacements) {
+  public @NotNull Component get(String path, Object... replacements) {
     Source source = config.getSource();
     String legacyResult = source.getString(path);
     if(legacyResult == null) {
       return Component.text(path);
     }
-    TextComponent result = (TextComponent) miniMessage.parse(path);
-    TextComponent finalResult = replacer.replace(result, replacements);
+    Component result = (TextComponent) miniMessage.parse(path);
+    Component finalResult = replacer.replace(result, replacements);
     config.getInterceptors()
       .forEach(interceptor -> interceptor.intercept(finalResult));
     return finalResult;
   }
 
   @Override
-  public @NotNull List<TextComponent> getMany(String path, Object... replacements) {
+  public @NotNull List<Component> getMany(String path, Object... replacements) {
     Source source = config.getSource();
     List<String> legacyResult = source.getStringList(path);
     if(legacyResult == null || legacyResult.isEmpty()) {
       return Collections.singletonList(Component.text(path));
     }
 
-    List<TextComponent> result = new ArrayList<>();
+    List<Component> result = new ArrayList<>();
     legacyResult.forEach(r -> {
       result.add((TextComponent) miniMessage.parse(r));
     });
--- bukkit-adventure/src/main/java/dev/henko/message/bukkit/AdventureMessengerProvider.java
+++ bukkit-adventure/src/main/java/dev/henko/message/bukkit/AdventureMessengerProvider.java
@@ -4,50 +4,51 @@ import dev.henko.message.api.Messenger;
 import dev.henko.message.api.MessengerConfig;
 import dev.henko.message.api.MessengerProvider;
 import dev.henko.message.api.impl.MessengerConfigImpl;
-import dev.henko.message.bukkit.messenger.AdventureMessenger;
+import dev.henko.message.adventure.messenger.AdventureMessenger;
 import dev.henko.message.bukkit.source.YamlSource;
 import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.platform.bukkit.BukkitAudiences;
-import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 public class AdventureMessengerProvider
-  implements MessengerProvider<TextComponent> {
+  implements MessengerProvider<Component> {
 
   private final Plugin plugin;
   private final String sourceName;
   private final BukkitAudiences bukkitAudiences;
   private final MiniMessage miniMessage;
+  private final LegacyComponentSerializer componentSerializer;
 
   public AdventureMessengerProvider(
     Plugin plugin,
     String sourceName,
     BukkitAudiences bukkitAudiences,
-    MiniMessage miniMessage
+    MiniMessage miniMessage,
+    LegacyComponentSerializer componentSerializer
+
   ) {
     this.plugin = plugin;
     this.sourceName = sourceName;
     this.bukkitAudiences = bukkitAudiences;
     this.miniMessage = miniMessage;
+    this.componentSerializer = componentSerializer;
   }
 
   @Override
-  public Messenger<TextComponent> get() {
-    MessengerConfig<TextComponent> config = new MessengerConfigImpl<>();
+  public Messenger<Component> get() {
+    MessengerConfig<Component> config = new MessengerConfigImpl<>();
     config.setSource(new YamlSource(plugin, sourceName))
       .addEntity(CommandSender.class, (entity, message) -> {
         if(entity instanceof Player) {
           Audience audience = bukkitAudiences.player((Player) entity);
           audience.sendMessage(message);
         } else {
-          StringBuilder legacyMessage = new StringBuilder(message.content());
-          message.children().forEach(c -> {
-            legacyMessage.append(((TextComponent) c).content());
-          });
-          entity.sendMessage(legacyMessage.toString());
+          entity.sendMessage(componentSerializer.serialize(message));
         }
       })
       .addEntity(Player.class, (entity, message) -> {
